f711af14a7dd6ec89409cef25783a3fd1e3539bb
vinodkumarkanchi <grahulbangalore@.com>
2023-08-01 07:43:24 +0100

    ql3 react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HeroDetail from './components/HeroDetail';
+import AllHeroes from './components/AllHeroes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <AllHeroes/>
+
+  );
+}
+
+export default App;
